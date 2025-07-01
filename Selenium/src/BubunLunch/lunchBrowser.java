48c35117e0cda5e249eed85d84e29f006661820d
AMULYA <AMULYA@DESKTOP-GE4GJ1U>
2025-07-01 13:15:30 +0530

    lunch added

---
+++ Selenium/src/BubunLunch/lunchBrowser.java
@@ -0,0 +1,13 @@
+package BubunLunch;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class lunchBrowser {
+	@Test
+	public void demo() {
+		WebDriver driver =  new ChromeDriver();
+	}
+
+}
